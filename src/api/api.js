7281ea6f21fe0c6dcbac80bf01d2f31ae5b2aea9
yashpal-nimesh-au2 <yash.nimesh456@gmail.com>
2019-10-11 11:26:33 +0530

    fetch api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 export default function apiCall(value,query) {
   var url,arr
 if(value==="Artist"){
-  url=`https://raw.githubusercontent.com/attainu/bravo-fdd-au2/yashpal/data/artist.json?token=AI7UY75NE62IAPVJZ55ASE25VBUIE`
+  url=`https://raw.githubusercontent.com/attainu/bravo-fdd-au2/yashpal/data/artist.json?token=AI7UY747IBONGDFXKIOGSIS5VFJ42`
 }
 else if(value==="getinfo"){
   url=`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${query}&api_key=271cbe482898bec42a2711e8df29b1e9&format=json`
